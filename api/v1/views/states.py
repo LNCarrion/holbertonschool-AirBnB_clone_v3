7443c95109f6e388b18820347abf23e0799e9aa9
AramisAra <martinezcruzaj@gmail.com>
2024-04-24 10:24:08 -0400

    plz work

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -4,8 +4,21 @@ from models import storage
 from models.state import State
 from api.v1.views import app_views
 
+
 @app_views.route('/states', methods=['GET', 'POST'])
 def states():
+    """
+    Retrieves the list of all State objects or creates a new State object
+
+    GET:
+        Returns a JSON representation of all State objects
+
+    POST:
+        Creates a new State object based on the JSON body of the request
+
+    Returns:
+        JSON representation of the State object(s) or an error message
+    """
     if request.method == 'GET':
         states = storage.all(State).values()
         return jsonify([state.to_dict() for state in states])
@@ -18,10 +31,28 @@ def states():
             abort(400, 'Missing name')
         new_state = State(**data)
         new_state.save()
-        return jsonify(new_state.to_dict()), 201
+
 
 @app_views.route('/states/<state_id>', methods=['GET', 'PUT', 'DELETE'])
 def state(state_id):
+    """
+    Retrieves, updates or deletes a State object based on its ID
+
+    Args:
+        state_id (str): The ID of the State object
+
+    GET:
+        Returns a JSON representation of the State object
+
+    PUT:
+        Updates the State object based on the JSON body of the request
+
+    DELETE:
+        Deletes the State object
+
+    Returns:
+        JSON representation of the State object or an error message
+    """
     state = storage.get(State, state_id)
     if not state:
         abort(404)
